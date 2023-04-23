90bd021b6335028f61be2bb3b094df10ba4a76f1
kitron96 <bartoszkitrys@gmail.com>
2023-04-23 16:02:49 +0200

    Update notify-title.blade.php

--- resources/views/components/notify-title.blade.php
+++ resources/views/components/notify-title.blade.php
@@ -1,7 +1,7 @@
 @props(['title'])
 
 <p @class([
-    'text-sm leading-5 font-medium capitalize',
+    'text-sm leading-5 font-medium',
     'text-slate-900 dark:text-gray-300' => config('notify.theme') === 'light',
     'text-white' => config('notify.theme') !== 'light',
 ])>
