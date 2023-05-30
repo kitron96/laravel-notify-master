11e7861bb3bbb2468ed6c8b00c916b4b47f1f143
kitron96 <bartoszkitrys@gmail.com>
2023-05-30 14:37:39 +0200

    Update LaravelNotifyServiceProvider.php

--- src/LaravelNotifyServiceProvider.php
+++ src/LaravelNotifyServiceProvider.php
@@ -4,6 +4,8 @@ namespace coderslab\Notify;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Jenssegers\Agent\Agent as Agent;
+
 
 final class LaravelNotifyServiceProvider extends ServiceProvider
 {
@@ -11,8 +13,16 @@ final class LaravelNotifyServiceProvider extends ServiceProvider
     {
         $this->registerBladeDirective();
         $this->registerPublishables();
+        
+        $Agent = new Agent();
+        if ($Agent->isMobile()) {
+            $viewPath = __DIR__.'/../resources/views/mobile';
+        } else {
+            $viewPath = __DIR__.'/../resources/views/web';
+        }
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'notify');
+
+        $this->loadViewsFrom(array($viewPath));
     }
 
     public function register(): void
@@ -38,7 +48,8 @@ final class LaravelNotifyServiceProvider extends ServiceProvider
     public function registerPublishables(): void
     {
         $this->publishes([
-            __DIR__.'/../public' => public_path('vendor/coderslab/laravel-notify'),
+            __DIR__.'/../public' => public_path('mobile/vendor/coderslab/laravel-notify'),
+            __DIR__.'/../public' => public_path('web/vendor/coderslab/laravel-notify'),
         ], 'notify-assets');
 
         $this->publishes([
