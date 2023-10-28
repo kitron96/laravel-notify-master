e8097da01138a373c1e59d4fe24bac64cae15675
Bartosz Kitrys <119293102+kitron96@users.noreply.github.com>
2023-10-28 18:51:51 +0200

    Update toast.blade.php

--- resources/views/notifications/toast.blade.php
+++ resources/views/notifications/toast.blade.php
@@ -12,7 +12,8 @@
             x-transition:leave-end="opacity-0"
             @class([
                 'pointer-events-auto w-full max-w-sm overflow-hidden shadow-lg rounded-lg border-2',
-                'bg-white dark:bg-[#222529]' => config('notify.theme') !== 'light',
+                'bg-white dark:bg-gray-800' => config('notify.theme') === 'light',
+                'bg-gray-800' => config('notify.theme') !== 'dark',
                 'border-green-500' => session()->get('notify.type') === 'success',
                 'border-yellow-500' => session()->get('notify.type') === 'warning',
                 'border-blue-500' => session()->get('notify.type') === 'info',
