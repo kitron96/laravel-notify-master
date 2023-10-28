9985f920c7a310e090dbbc89e8338a6cbc528f2b
Bartosz Kitrys <119293102+kitron96@users.noreply.github.com>
2023-10-28 18:44:17 +0200

    1

--- resources/views/notifications/smiley.blade.php
+++ resources/views/notifications/smiley.blade.php
@@ -1,7 +1,7 @@
-@if (session()->get('notify.model') === 'connect')
-    <div class="notify fixed inset-0 flex items-end justify-end px-1 py-1 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+@if (session()->get('notify.model') === 'smiley')
+    <div class="notify fixed inset-0 flex items-end justify-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
         <div
-            x-data="{ show: @if(session()->get('notify.model') === 'connect') true @else false @endif }"
+            x-data="{ show: @if(session()->get('notify.model') === 'smiley') true @else false @endif }"
             x-init="setTimeout(() => { show = true }, 750)"
             x-show="show"
             x-transition:enter="transform ease-out duration-300 transition"
@@ -11,34 +11,22 @@
             x-transition:leave-start="opacity-100"
             x-transition:leave-end="opacity-0"
             @class([
-                'pointer-events-auto max-w-sm w-full shadow-lg rounded-lg border-t-4',
+                'pointer-events-auto max-w-sm w-full shadow-lg rounded-lg',
                 'bg-white' => config('notify.theme') === 'light',
-                'bg[#fff] dark:bg-[#17181a]' => config('notify.theme') !== 'light',
-                'border-green-600' => session()->get('notify.type') === 'success',
-                'border-red-600' => session()->get('notify.type') === 'error',
+                'bg-slate-800' => config('notify.theme') !== 'light',
             ])
         >
             <div class="relative rounded-lg shadow-xs overflow-hidden">
                 <div class="p-4">
                     <div class="flex items-start">
-                        @if(session()->get('notify.type') === 'success')
-                            <div class="inline-flex items-center bg-gradient-to-r from-green-600 to-green-800 p-2 text-white text-sm rounded-full shrink-0">
-                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
-                                </svg>
-                            </div>
-                        @endif
-                        @if(session()->get('notify.type') === 'error')
-                            <div class="inline-flex items-center bg-gradient-to-r from-red-600 to-red-800 p-2 text-white text-sm rounded-full shrink-0">
-                                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                                </svg>
-                            </div>
-                        @endif
+                        <div class="inline-flex items-center text-white text-2xl shrink-0">
+                            @if(session()->get('notify.type') === 'success') <span>👍</span> @endif
+                            @if(session()->get('notify.type') === 'error') <span>🙅🏽‍♂️</span> @endif
+                        </div>
                         <div class="ml-6 w-0 flex-1">
                             <x-notify::notify-content :content="session()->get('notify.message')" />
                         </div>
-                        <div class="ml-4 flex-shrink-0 flex">
+                        <div class="ml-4 shrink-0 flex">
                             <x-notify::button />
                         </div>
                     </div>
--- resources/views/notifications/toast.blade.php
+++ resources/views/notifications/toast.blade.php
@@ -1,8 +1,8 @@
-@if (session()->get('notify.model') === 'connect')
-    <div class="notify fixed inset-0 flex items-end justify-end px-1 py-1 pointer-events-none sm:p-6 sm:items-start sm:justify-end">
+@if (session()->get('notify.model') === 'toast')
+    <div class="notify fixed inset-0 z-10 flex items-end justify-end px-2 py- pointer-events-none sm:p-6 sm:items-start sm:justify-end">
         <div
-            x-data="{ show: @if(session()->get('notify.model') === 'connect') true @else false @endif }"
-            x-init="setTimeout(() => { show = true }, 750)"
+            x-data="{ show: @if(session()->get('notify.model') === 'toast') true @else false @endif }"
+            x-init="setTimeout(() => { show = true }, 500)"
             x-show="show"
             x-transition:enter="transform ease-out duration-300 transition"
             x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
@@ -11,34 +11,46 @@
             x-transition:leave-start="opacity-100"
             x-transition:leave-end="opacity-0"
             @class([
-                'pointer-events-auto max-w-sm w-full shadow-lg rounded-lg border-t-4',
-                'bg-white' => config('notify.theme') === 'light',
-                'bg[#fff] dark:bg-[#17181a]' => config('notify.theme') !== 'light',
-                'border-green-600' => session()->get('notify.type') === 'success',
-                'border-red-600' => session()->get('notify.type') === 'error',
+                'pointer-events-auto w-full max-w-sm overflow-hidden shadow-lg rounded-lg border-2',
+                'bg-white dark:bg-gray-800' => config('notify.theme') === 'light',
+                'bg-slate-800' => config('notify.theme') !== 'light',
+                'border-green-500' => session()->get('notify.type') === 'success',
+                'border-yellow-500' => session()->get('notify.type') === 'warning',
+                'border-blue-500' => session()->get('notify.type') === 'info',
+                'border-red-500' => session()->get('notify.type') === 'error',
             ])
         >
             <div class="relative rounded-lg shadow-xs overflow-hidden">
                 <div class="p-4">
-                    <div class="flex items-start">
+                    <div class="flex items-center">
                         @if(session()->get('notify.type') === 'success')
                             <div class="inline-flex items-center bg-gradient-to-r from-green-600 to-green-800 p-2 text-white text-sm rounded-full shrink-0">
-                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                                 </svg>
                             </div>
+                        @endif
+                        @if(session()->get('notify.type') === 'warning')
+                            <svg class="w-6 h-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                            </svg>
+                            @endif
+                        @if(session()->get('notify.type') === 'info')
+                            <svg class="w-6 h-6 text-blue-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                            </svg>
                         @endif
                         @if(session()->get('notify.type') === 'error')
                             <div class="inline-flex items-center bg-gradient-to-r from-red-600 to-red-800 p-2 text-white text-sm rounded-full shrink-0">
-                                <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </div>
                         @endif
-                        <div class="ml-6 w-0 flex-1">
+                        <div class="ml-4 w-0 flex-1">
                             <x-notify::notify-content :content="session()->get('notify.message')" />
                         </div>
-                        <div class="ml-4 flex-shrink-0 flex">
+                        <div class="ml-4 flex shrink-0">
                             <x-notify::button />
                         </div>
                     </div>
